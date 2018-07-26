11e27ccfeb6dfbb33b385bddce3e9ce82d5f13dd
junli <lijun@sge.com.cn>
2018-07-26 08:44:06 +0800

    10.3.2 Edit Profile - Admin Page

--- app/main/views.py
+++ app/main/views.py
@@ -3,19 +3,16 @@ from flask import render_template, session, redirect, url_for, abort, flash
 from flask_login import login_user, logout_user, login_required, current_user
 
 from app import db
-from app.models import User
+from app.models import User, Role
 from . import main
-from .forms import EditProfileForm
+from .forms import EditProfileForm, EditProfileAdminForm
+from app.decorators import admin_required
 
 
 
 @main.route('/',methods=['GET','POST'])
 def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        return redirect(url_for('.index'))
-    return render_template('index.html', form=form, name=session.get('name'), known=session.get('known',False),
-                           current_time= datetime.now())
+    return render_template('index.html')
 
 
 @main.route('/user/<username>')
@@ -42,3 +39,29 @@ def edit_profile():
     form.about_me.data = current_user.about_me
     return render_template('edit-profile.html', form=form)
 
+
+@main.route('/user/edit-profile/<int:id>',methods=['GET','POST'])
+@login_required
+@admin_required
+def edit_profile_admin(id):
+    user = User.query.get_or_404(id)
+    form = EditProfileAdminForm(user=user)
+    if form.validate_on_submit():
+        user.username = form.username.data
+        user.email = form.email.data
+        user.confirmed = form.confirmed.data
+        user.name = form.name.data
+        user.about_me = form.about_me.data
+        user.location = form.location.data
+        user.role = Role.query.get(form.role.data)
+        db.session.add(user)
+        flash('用户信息更新完成')
+        return redirect(url_for('.user', username=user.username))
+    form.email.data = user.email
+    form.username.data = user.username
+    form.name.data = user.name
+    form.location.data = user.location
+    form.about_me.data = user.about_me
+    form.confirmed.data = user.confirmed
+    form.role.data = user.role_id
+    return render_template('edit-profile.html', form=form, user=user)
