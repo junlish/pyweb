4805cdacf5e70ab88da0e639405b71b1d63fff73
junli <lijun@sge.com.cn>
2018-08-02 16:51:57 +0800

    12.3 show followed posts in index page

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import render_template, session, redirect, url_for, abort, flash, request,current_app
+from flask import render_template, session, redirect, url_for, abort, flash, request,current_app, make_response
 from flask_login import login_user, logout_user, login_required, current_user
 
 from app import db
@@ -19,11 +19,36 @@ def index():
         db.session.add(post)
         return redirect(url_for('.index'))
 
+    show_followed = False
+    if current_user.is_authenticated:
+        show_followed = bool(request.cookies.get('show_followed',''))
+
+    if show_followed:
+        query = current_user.followed_posts
+    else:
+        query = Post.query
+
     page = request.args.get('page',1, type=int)
-    pagination = Post.query.order_by(Post.timestamp.desc()).paginate(page,
+    pagination = query.order_by(Post.timestamp.desc()).paginate(page,
                 per_page=current_app.config['FLASKY_POSTS_PER_PAGE'], error_out=False)
     posts = pagination.items
-    return render_template('index.html', form = form, posts=posts, pagination=pagination)
+    return render_template('index.html', form = form, posts=posts, pagination=pagination, show_followed=show_followed)
+
+
+@main.route('/all')
+@login_required
+def show_all():
+    resp = make_response(redirect(url_for('.index')))
+    resp.set_cookie('show_followed', '',max_age=30*24*60*60)
+    return resp
+
+
+@main.route('/followed')
+@login_required
+def show_followed():
+    resp = make_response(redirect(url_for('.index')))
+    resp.set_cookie('show_followed', '1',max_age=30*24*60*60)
+    return resp
 
 
 @main.route('/user/<username>')
@@ -131,6 +156,7 @@ def unfollow(username):
     else:
         flash("您并未关注{},无法操作".format(username))
         return redirect(url_for('.user', username=username))
+
 
 @main.route('/followers/<username>')
 def followers(username):
