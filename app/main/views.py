00085fa9b5f973bd8979e90b1ae5bc6b02e2cb89
junli <lijun@sge.com.cn>
2018-08-16 16:55:32 +0800

    16 performance logger

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from flask import render_template, session, redirect, url_for, abort, flash, request,current_app, make_response
 from flask_login import login_user, logout_user, login_required, current_user
+from flask_sqlalchemy import get_debug_queries
 
 from app import db
 from app.models import User, Role, Comment
@@ -229,3 +230,16 @@ def moderate_disable(id):
     comment.disabled = True
     db.session.add(comment)
     return redirect(url_for('.moderate',  page= request.args.get('page',1,type=int)))
+
+
+@main.after_app_request
+def after_request(response):
+    for query in get_debug_queries():
+        if query.duration >= current_app.config['FLASKY_SLOW_DB_QUERY_TIME']:
+            current_app.logger.warning(
+                'Slow query: %s\nParameters: %s\nDuration: %fs\nContext: %s\n'
+                % (query.statement, query.parameters, query.duration,
+                   query.context))
+
+    return response
+
